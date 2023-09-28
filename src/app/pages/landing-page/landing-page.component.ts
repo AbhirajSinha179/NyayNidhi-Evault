1d514091e8c60e753cb968844cd7bb96df72e448
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2023-09-29 04:16:53 +0530

    string input in form

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent{
 
+  constructor() { }
+  userID="";
+  ngOnInit(): void {
+  }
+  getID(val:string){
+    this.userID=val;
+  }
 }
